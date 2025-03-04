5d11c25980d68834fc5639424f3396c1e8059c01
Francesco-Napolitano <francesco.46napolitano@gmail.com>
2025-03-04 17:32:46 +0100

    Initial commit

---
+++ src/main/java/it/finalproject/auto_collection/AutoCollectionApplication.java
@@ -0,0 +1,13 @@
+package it.finalproject.auto_collection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoCollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/finalproject/auto_collection/AutoCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package it.finalproject.auto_collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
